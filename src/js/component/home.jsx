59cb3c499c09a43004ad2992de402edd9463e016
levanmamuladze <levanmamoulatze@gmail.com>
2023-04-14 17:45:50 +0000

    To-do list done

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,83 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [newTodo, setNewTodo] = useState("");
+  const [todoList, setTodoList] = useState([]);
+  const [hoveredIndex, setHoveredIndex] = useState(null);
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
+  const getTodos = async () => {
+    const response = await fetch(
+      "https://assets.breatheco.de/apis/fake/todos/user/levan"
+    );
+    const data = await response.json();
+    setTodoList(data);
+  };
+
+  const addTodo = () => {
+    if (newTodo !== "") {
+      const checkLetter =
+        newTodo.charAt(0).toUpperCase() + newTodo.slice(1).toLowerCase();
+      const newTodoItem = { label: checkLetter };
+      const check = todoList.find(todo => todo.label === newTodoItem.label);
+      if (check) {
+        alert("You can't add two same tasks!");
+      } else {
+        setTodoList([...todoList, newTodoItem]);
+        setNewTodo("");
+      }}}
+  return (
+    <div className=" container text-center justify-content-center container pt-5 main ">
+      <h1>Add tasks to your To-Do-List</h1>
+      <input
+        value={newTodo}
+        placeholder="Add a new todo"
+        onChange={(e) => {
+          setNewTodo(e.target.value);
+        }}
+        onKeyUp={(e) => {
+          if (e.key === "Enter") {
+            addTodo();
+          }
+        }}
+      />
+      <ul className="mx-auto p-2">
+        {todoList.map((todo, index) => {
+          return (
+            <li
+              className="col-12 d-flex liDesing"
+              key={index}
+              onMouseEnter={() => setHoveredIndex(index)}
+              onMouseLeave={() => setHoveredIndex(null)}
+            >
+              <div className="col-1 text-end">-</div>
+              <div
+                className="col-10 text-start"
+                style={{ wordBreak: "break-all" }}
+              >
+                {todo.label}
+              </div>
+              <div className="col-1 text-end ">
+                {hoveredIndex === index && (
+                  <button
+                    className="delete-button btn btn-danger button-icon fas fa-trash-alt p-1 "
+                    onClick={() => {
+                      const newList = [...todoList];
+                      newList.splice(index, 1);
+                      setTodoList(newList);
+                    }}
+                  ></button>
+                )}
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default Home;
